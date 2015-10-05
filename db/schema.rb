41f960226cdeae24a65361de92834c2fa7618cd4
Nizar Triki <triki.nizar@gmail.com>
2015-10-05 18:13:56 -0400

    color troubeshoot

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002183458) do
+ActiveRecord::Schema.define(version: 20151003171338) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151002183458) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
   end
 
 end
